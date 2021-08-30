e308cff7568461f72e02df05f4f6d009ae917c1e
jeongsuchoi <jscinsoul@gmail.com>
2021-08-30 20:40:01 +0900

    after vacation. commit now i'm in inheritance part.

---
+++ app/src/main/java/com/example/coreprinciple/ConclusionPointer.java
@@ -0,0 +1,27 @@
+package com.example.coreprinciple;
+
+public class ConclusionPointer {
+
+    // 외울 문장
+    // 참조는 결국 포인터와 같은 것이므로 포인터가 없다는 설명은 어불성설이다.
+
+    // java.sun.com/ -> faq
+
+    // Of all the misconceptions about the Java Programming language, this is the most egregious.
+
+    // Far from not having pointers, object-oriented programming is conducted in the Java Programming language
+
+    // exclusively with pointers. In other words, objects are only ever accessed through pointers,
+
+    // never directly.
+
+    // The pointers are termed "references" and they are automatically dereferenced for you.
+
+    // "An Object is class instance or an array. The reference values (often just references) are
+
+    // pointers to the objects"
+
+    // Java Language Specification, section 4.3.1. emphasis is in the original text
+
+    // Any book that claims Java does not have pointers is inconsistent with the Java reference specification.
+}
